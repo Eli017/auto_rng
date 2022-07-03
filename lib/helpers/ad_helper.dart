4eef7a9c9b2945a74e12b8a142f01c473d1e35c6
Eli Sokelalnd <elisokeland@gmail.com>
2022-07-03 01:59:14 -0400

    Created ad helper class

---
+++ lib/helpers/ad_helper.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+class AdHelper {
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return 'ca-app-pub-1655051670432168/1296773130';
+    } else if (Platform.isIOS) {
+      return 'ca-app-pub-1655051670432168/1810658894';
+    } else {
+      throw new UnsupportedError('Unsupported platform');
+    }
+  }
+}
