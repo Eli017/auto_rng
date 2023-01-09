5f8c7bdd2020865bb0ee98f62855941f78a7be9a
Eli Sokeland <elisokeland@yahoo.com>
2023-01-08 23:29:36 -0500

    Added library for streaming time values

--- lib/widgets/duration_selector.dart
+++ lib/widgets/duration_selector.dart
@@ -27,6 +27,7 @@ class _DurationSelectorState extends State<DurationSelector>  {
     int minuteMilliseconds = convertMinutesToMilliseconds(minutes);
     int secondMilliseconds = convertSecondsToMilliseconds(seconds);
     int newMilliseconds = minuteMilliseconds + secondMilliseconds + milliseconds;
+    print(newMilliseconds);
     setState(() => currentDuration = newMilliseconds);
     //Every frame within 60fps is approximately 16.3 milliseconds.
     //Therefore, this app must have a function delay of at least 17 seconds.
